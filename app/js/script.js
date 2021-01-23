7a8e9a5e92a265512b7cf318eaf2b30b23246868
BlackSuperDev <oyeniyi4real@gmail.com>
2021-01-23 11:40:47 +0100

    mobile overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const hamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+hamburger.addEventListener('click', function(){
+    console.log('hello clicker')
+    if(header.classList.contains('open')){
+        // close hamburger
+        header.classList.remove('open')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+    }
+    else{
+        header.classList.add('open')
+        overlay.classList.add('fade-in')
+        overlay.classList.remove('fade-out')
+    }
+})
